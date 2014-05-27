9b5e63c93f67508741b1ddb3305d925e9c7d973f
twashtri <dreamwalker.eleventh@gmail.com>
2014-05-27 22:00:08 +0530

    initial code commit

---
+++ src/main/java/org/automata/simulation/Cell.java
@@ -0,0 +1,18 @@
+package org.automata.simulation;
+
+public class Cell {
+  private char data;
+
+  public Cell(char data) {
+    super();
+    this.data = data;
+  }
+
+  public char getData() {
+    return data;
+  }
+
+  public void setData(char data) {
+    this.data = data;
+  }
+}
